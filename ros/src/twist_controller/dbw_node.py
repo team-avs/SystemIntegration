6a8bae88a373a83cf717db5f15aa874902c2dc2b
Mate Bartalos <bartmate@gmail.com>
2017-09-08 12:12:31 -0700

    Fix the sign issue. Now the car follow the path.

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -88,7 +88,7 @@ class DBWNode(object):
 ###More investigation or maybe newer simulator is needed
 
             throttle, brake, angle = self.controller.control(self.trgtv,self.trgtav,self.currv)
-            self.publish(throttle, brake, -angle*180./3.14159265/25.0)
+            self.publish(throttle, brake, angle*180./3.14159265/5.0)
             
             #rospy.loginfo('---------------------------------------------------------------------------')
             #rospy.loginfo('DBW Loop In : cv:%s;cav:%s;tv:%s;tav:%s;',self.currv,self.currav,self.trgtv,self.trgtav)
