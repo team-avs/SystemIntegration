5dfd14a3a87baae7cc2ef6fb796d84e994f9cbb0
Maurizio Pinto <maurizio.pinto@gmail.com>
2017-09-09 13:08:30 -0700

    Set # of waypoint ahead to 20

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -25,7 +25,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 20 # Number of waypoints we will publish. You can change this number
 
 SPEED = 5 # ~10MPH
 
