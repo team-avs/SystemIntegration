ee0be53779047d33399a0c524e1726e6dc57ec2b
Victor Costa <vfcosta@gmail.com>
2017-09-10 10:11:54 +0100

    tl_detector: display classifier metrics

--- ros/src/tl_detector/tl_detector_train.py
+++ ros/src/tl_detector/tl_detector_train.py
@@ -1,8 +1,11 @@
 import os
 import numpy as np
 import cv2
+import math
+import sys
+import operator
 from keras.optimizers import Adam
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
@@ -14,6 +17,10 @@ SCALE = 4
 CLASSES = {'red': 0, 'yellow': 1, 'green': 2, 'nolight': 3, 'unidentified': 4}
 
 class TLDetectorTrain(object):
+
+    def __init__(self):
+        self.batch_size = 10
+
 
     def create_model(self):
         self.model = Sequential()
@@ -51,7 +58,7 @@ class TLDetectorTrain(object):
                 img = cv2.imread(image[0])
                 X.append(self.preprocess(img))
                 Y.append(image[1])
-                if len(X) >= batch_size:
+                if len(X) >= batch_size or len(X) >= len(images):
                     yield (np.array(X), to_categorical(np.array(Y), len(CLASSES)))
                     X, Y = [], []
 
@@ -60,18 +67,43 @@ class TLDetectorTrain(object):
         self.create_model()
         optimizer = Adam(lr=0.00001)
         self.model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
-        batch_size = 10
         images = self.list_images()
         np.random.shuffle(images)
         train_images = images[:int(len(images)*0.8)]
         validation_images = images[int(len(images)*0.8):]
         print("train size: %d, validation size: %d" % (len(train_images), len(validation_images)))
-        self.model.fit_generator(self.generate_data(train_images, batch_size=batch_size), steps_per_epoch=len(train_images)/batch_size, epochs=12)
+        self.model.fit_generator(self.generate_data(train_images, batch_size=self.batch_size), steps_per_epoch=math.ceil(len(train_images)/float(self.batch_size)), epochs=12)
         self.model.save(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'model.h5'))
-        metrics = self.model.evaluate_generator(self.generate_data(validation_images, batch_size=batch_size), steps=len(validation_images)/batch_size)
+        metrics = self.model.evaluate_generator(self.generate_data(validation_images, batch_size=self.batch_size), steps=math.ceil(len(validation_images)/float(self.batch_size)))
         print(metrics)
+
+
+    def load(self):
+        self.model = load_model(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'model.h5'))
+
+
+    def display_metrics(self):
+        images = self.list_images()
+        np.random.shuffle(images)
+        validation_images = images[:10]
+        metrics = self.model.evaluate_generator(self.generate_data(validation_images, batch_size=self.batch_size), steps=math.ceil(len(validation_images)/float(self.batch_size)))
+        print(metrics)
+        for image in validation_images[:10]:
+            class_index, probability = self.predict(cv2.imread(image[0]))
+            print("class: %d, predicted class: %d, probability: %f, image: %s" % (image[1], class_index, probability, image[0].replace(DATA_PATH, '')))
+
+
+    def predict(self, image):
+        predictions = self.model.predict(np.array([self.preprocess(image)]), batch_size=1)[0]
+        return max(enumerate(predictions), key=operator.itemgetter(1))
 
 
 if __name__ == '__main__':
     tl_detector_train = TLDetectorTrain()
-    tl_detector_train.train()
+    if sys.argv[1] == '-t':
+        tl_detector_train.train()
+    elif sys.argv[1] == '-m':
+        tl_detector_train.load()
+        tl_detector_train.display_metrics()
+    else:
+        print("Usage: use -t for training and -m for current metrics")
